f30b73efd2e40560d8d85639635c07e53b7922a6
frank pagan <frank@cocreate.app>
2021-09-14 03:55:40 +0000

    fix: add cocreate.app to domains

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 "name": "index.html",
                 "path": "/docs/tui-image-editor/index.html",
                 "domains": [
+                    "cocreate.app",
                     "general.cocreate.app"
                 ],
                 "directory": "/docs/tui-image-editor",
