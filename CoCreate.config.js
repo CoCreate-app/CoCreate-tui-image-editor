689c0516d53224ad4d20bbde841591329d83b4d7
frankpagan <frank@cocreate.app>
2023-08-03 17:47:17 -0600

    fix: crud attributes renamed

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -4,8 +4,8 @@ module.exports = {
     "host": "",
     "sources": [
         {
-            "collection": "files",
-            "document": {
+            "array": "files",
+            "object": {
                 "_id": "637ca69f50234ef1671ce337",
                 "name": "index.html",
                 "path": "/docs/tui-image-editor/index.html",
