a29550d64a605dd958577ad68db93f60cfb628ea
frankpagan <frank@cocreate.app>
2023-09-17 19:11:38 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "637ca69f50234ef1671ce337",
                 "name": "index.html",
-                "path": "/docs/tui-image-editor/index.html",
+                "path": "/docs/tui-image-editor",
+                "pathname": "/docs/tui-image-editor/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/tui-image-editor",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "tui-image-editor",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
